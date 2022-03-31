b3abe1605d4a5bbed70c87ed2dba50e8d1d960cc
Nathan Yee <nyee@Nathans-MBP.attlocal.net>
2022-03-30 18:33:23 -0700

    started product section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import NavBar from './components/NavBar';
-
+import ProductDetails from './components/ProductDetails';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <ProductDetails />
     </div>
   );
 }
--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,8 +1,18 @@
+import './ProductDetails.css'
+import product1 from '../img/image-product-1.jpg'
+
 function ProductDetails(props) {
 
     return(
         <div className="container">
-
+            <div className="col">
+                <img src={product1} className='product1' />
+            </div>
+            <div className="col">
+                <div className='productdetails'>
+                    <h1>Sneaker Company</h1>
+                </div>
+            </div>
         </div>
     );
 }
